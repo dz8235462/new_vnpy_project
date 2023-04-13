165f3650c21846b7c808913dda93c6b7b5fc99ca
dongzhi <554757915@qq.com>
2023-04-13 14:47:53 +0800

    04 macd_hist

--- strategies/base_cta_strategy.py
+++ strategies/base_cta_strategy.py
@@ -9,13 +9,7 @@ from vnpy.trader.object import (
 from vnpy.trader.utility import BarGenerator, ArrayManager
 from vnpy_ctastrategy import CtaTemplate, StopOrder
 from vnpy_ctastrategy.backtesting import BacktestingEngine
-from vnpy.trader.constant import Offset, Direction
 from vnpy.trader.object import AccountData
-
-
-GLOBAL_SETTINGS = {
-    "BACK_TESTING_DATA_SAVE": False
-}
 
 
 class BaseCtaStrategy(CtaTemplate):
@@ -29,7 +23,7 @@ class BaseCtaStrategy(CtaTemplate):
     # 定义参数
     bar_window = 1  # k线周期
     need_stop = 1  # 0/1是否需要止损
-    trailing_percent = 5.0  # 百分比移动止损，0.8%即止损
+    trailing_percent = 5.0  # 百分比移动止损
     last_stop_order_id = None
 
     # 移动止损
@@ -40,10 +34,12 @@ class BaseCtaStrategy(CtaTemplate):
 
     def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
         """"""
+        # 调用父类初始化方法，确保setting被正确注入
         super().__init__(cta_engine, strategy_name, vt_symbol, setting)
-        # 参数
+        # 参数打印
         print("%s, %s : %s" % (datetime.now(),strategy_name, str(setting)))
         # K线合成器：从Tick合成分钟K线用
+        # on_bar_m函数用于提供m分钟级别k线回调，默认推荐self.bar_window=1即可
         self.bg = BarGenerator(self.on_bar,
                                window=self.bar_window,
                                on_window_bar=self.on_bar_m)
@@ -63,7 +59,7 @@ class BaseCtaStrategy(CtaTemplate):
         当策略被初始化时调用该函数。
         """
         # 加载历史数据用于初始化回放
-        self.load_bar(60, use_database=True)
+        self.load_bar(100, use_database=True)
         self.output("策略初始化")
 
     def on_start(self):
@@ -99,16 +95,27 @@ class BaseCtaStrategy(CtaTemplate):
         pass
 
     def stop_order(self, bar: BarData, need_cancel_all=False, do_stop=True, tailing_ratio=1.0):
+        """
+        :param bar: 当前分钟k线
+        :param need_cancel_all: 是否在执行止损前调用cancel_all方法
+        :param do_stop: 是否立刻执行止损，在部分场景中，止损价格可能与策略反手价格不一致，可将结果返回，交由策略自身择优选择止损点位
+        :param tailing_ratio: 价格波动调整系数，用于策略自身根据需要动态调整止损百分比
+        :return: 1.是否已到止损点位 2.止损执行价格
+        """
         self.need_stop_for_now = False
+        # 是否策略初次启动，用于立刻初始化止损价格
         first_after_start = self.stop_price is None
         self.stop_price = None
+        # 持仓为0，重置行情最高点和最低点
         if self.pos == 0:
             self.intraTradeHigh = 0
             self.intraTradeLow = float('inf')
         if self.need_stop and self.pos != 0:
             # 移动止损策略
             if self.pos > 0:
+                # 多单持仓时，动态记录最低价，便于反手时使用最新价格开始统计
                 self.intraTradeLow = bar.low_price
+                # 最高价突破，同步移动止损点位
                 if bar.high_price > self.intraTradeHigh or first_after_start:
                     # 撤销原有所有单
                     if need_cancel_all:
@@ -119,6 +126,7 @@ class BaseCtaStrategy(CtaTemplate):
                     self.intraTradeHigh = max(self.intraTradeHigh, bar.high_price)
                     # 计算止损价位
                     self.longStop = self.intraTradeHigh * (1 - tailing_ratio * self.trailing_percent / 100)
+                    # 挂条件单，确保止损价触发时尽快成交
                     if do_stop:
                         self.output("stop order sell on longStop:%s, high:%s" % (self.longStop, self.intraTradeHigh))
                         self.last_stop_order_id = self.sell(self.longStop, self.pos, stop=True)
@@ -144,7 +152,7 @@ class BaseCtaStrategy(CtaTemplate):
         return self.need_stop_for_now, self.stop_price
 
     def on_bar_m(self, bar: BarData):
-        """To be overrided"""
+        """策略逻辑实现，子类重写这个方法即可 """
         pass
 
     def on_order(self, order: OrderData):
--- strategies/macd_hist_strategy.py
+++ strategies/macd_hist_strategy.py
@@ -1,4 +1,3 @@
-import scipy.special
 from vnpy.trader.object import (
     BarData,
 )
@@ -15,21 +14,17 @@ class MacdHistStrategy(BaseCtaStrategy):
 
     # 定义参数
     bar_window = 1  # k线间隔，单位：分钟
-    fast_window = 22  # 短期均线长度
-    mid_window = 44  # 短期均线长度
-    slow_window = 80  # 长期均线长度
+    fast_window = 40  # 短期均线长度
+    mid_window = 80  # 中期均线长度
 
     # 定义变量
     fast_ma0 = 0.0
     fast_ma1 = 0.0
     mid_ma0 = 0.0
     mid_ma1 = 0.0
-    slow_ma0 = 0.0
-    slow_ma1 = 0.0
 
     # 添加参数和变量名到对应的列表
-    parameters = ["need_stop", "trailing_percent", "bar_window", "fast_window", "mid_window", "slow_window",
-                  "percent", 'capital', 'size']
+    parameters = ["need_stop", "trailing_percent", "bar_window", "fast_window", "mid_window"]
     variables = ["intraTradeHigh", "intraTradeLow", "capital"]
 
     def __init__(self, cta_engine, strategy_name, vt_symbol, setting):
@@ -40,15 +35,20 @@ class MacdHistStrategy(BaseCtaStrategy):
 
         self.output("on_bar_m : %s" % str(bar))
         """
-        通过该函数收到新的1分钟K线推送。
+        通过该函数处理策略逻辑，MACD柱状线
         """
         # 更新K线到时间序列容器中
         self.dbg.update_bar(bar)
 
         # 若缓存的K线数量尚不够计算技术指标，则直接返回
-        if not self.dbg.is_inited(30):
+        # 这里30主要是为了计算MACD的26日均线，大于26即可。
+        if not self.dbg.is_inited(self.mid_window):
             # print("not inited")
             return
+
+        # 移动止损
+        self.stop_order(bar)
+
         # 计算快速均线
         fast_ma = self.dbg.sma(self.fast_window, array=True)
         self.fast_ma0 = fast_ma[-1]  # T时刻数值
@@ -57,97 +57,85 @@ class MacdHistStrategy(BaseCtaStrategy):
         mid_ma = self.dbg.sma(self.mid_window, array=True)
         self.mid_ma0 = mid_ma[-1]  # T时刻数值
         self.mid_ma1 = mid_ma[-2]  # T-1时刻数值
-        # 计算慢速均线
-        slow_ma = self.dbg.sma(self.slow_window, array=True)
-        self.slow_ma0 = slow_ma[-1]
-        self.slow_ma1 = slow_ma[-2]
-        # print("self.fast_ma0 : %s" % self.fast_ma0)
-        self.output("self.fast_ma1 : %s" % self.fast_ma1)
-        self.output("self.mid_ma0 : %s" % self.mid_ma0)
-        self.output("self.mid_ma1 : %s" % self.mid_ma1)
-        self.output("self.slow_ma0 : %s" % self.slow_ma0)
-        self.output("self.slow_ma1 : %s" % self.slow_ma1)
+
         # 计算macd
         macd_macd, macd_signal, macd_hist = self.dbg.macd(12, 26, 9, array=True)
-        macd_fast_ma0 = macd_macd[-1]
-        macd_fast_ma1 = macd_macd[-2]
 
-        macd_slow_ma0 = macd_signal[-1]
-        macd_slow_ma1 = macd_signal[-2]
-
-        # 判断是否金叉
+        # 判断是否短期均线高于中期均线，即上升趋势
         cross_1 = (self.fast_ma0 > self.mid_ma0)
-        # 判断是否死叉
-        cross_2 = (self.mid_ma0 > self.slow_ma0)
 
-        # 判断是否金叉
-        macd_cross_over = (macd_fast_ma0 > macd_slow_ma0)
-
+        # 目前尚未进行资金管理，使用固定手数开仓
         volume = 20
 
+        # 使用两个数组记录柱状线，长度可配置，便于判断两个数组的斜率是否一致
         macd_hist_trend = []
         macd_hist_trend_before = []
         new_trend_len = 2
         old_trend_len = 3
+        # 将对应柱状线放入前后两个列表中
         for i in range(new_trend_len):
             macd_hist_trend.append(macd_hist[-1 - i] - macd_hist[-2 - i])
         for i in range(old_trend_len):
             macd_hist_trend_before.append(macd_hist[-1 - new_trend_len - i] - macd_hist[-2 - new_trend_len - i])
+        # 分别记录转折点前后斜率是否上升/下降
         trend_up = len([v for v in macd_hist_trend if v > 0]) == len(macd_hist_trend)
         trend_down = len([v for v in macd_hist_trend if v < 0]) == len(macd_hist_trend)
         trend_before_up = len([v for v in macd_hist_trend_before if v > 0]) == len(macd_hist_trend_before)
         trend_before_down = len([v for v in macd_hist_trend_before if v < 0]) == len(macd_hist_trend_before)
 
-        # print(volume)
+        # 记录逻辑仓位，target_pos=-1/0/1分别代表开空/平仓/开多
         pos_changed = False
         target_pos = 0
-        if not cross_2 and macd_hist[-1] < 0 and trend_before_down and trend_up:
+        # 最后一根柱状线<0，且斜率先降后升，开多
+        if not cross_1 and macd_hist[-1] < 0 and trend_before_down and trend_up:
             target_pos = 1
             pos_changed = True
-        if cross_2 and macd_hist[-1] > 0 and trend_before_up and trend_down:
+        # 最后一根柱状线>0，且斜率先升后降，开空
+        if cross_1 and macd_hist[-1] > 0 and trend_before_up and trend_down:
             target_pos = -1
             pos_changed = True
 
-        # if pos_changed:
-        #     current_time = bar.datetime.time().hour
-        #     if current_time < 9:
-        #         current_time = 9
-        #     day_open, day_close = 9, 15
-        #     expit_signal = scipy.special.expit(current_time - day_open)
-        #     target_pos *= expit_signal
-
+        # 如果现有仓位与目标仓位相反，先单独平仓，等待下一分钟再执行开仓
+        # 避免同时进行两笔交易可能导致的回调异常
         if self.pos * target_pos < 0:
             target_pos = 0
 
-        # self.stop_order(bar)
 
+        # 计算最终目标仓位
         target_volumn = int(volume * target_pos)
+        # 异常数据过滤，如目标仓位方向一致，但新仓位较低，则不做任何操作
+        # 当引入手数计算以后，可能会出现手数随其他因素波动情况，此时需要避免频繁操作
         if self.pos * target_pos > 0 and abs(self.pos) >= abs(target_volumn):
             return
-        # 如果发生了金叉
+        # 仓位发生变化，计算差额
         change_volumn = target_volumn - self.pos
-        # print("%s,%s,%s" % (target_pos, change_volumn, self.pos))
         if change_volumn == 0:
             return
+        # 逻辑仓位未变化直接忽略，减少运算与操作次数
         if not pos_changed:
             return
+        # 取消所有未成交的指令
         self.cancel_all()
+        # 多单持仓，且目标仓位增加，开多
         if self.pos >= 0 and change_volumn > 0:
-            # 为了保证成交，在K线收盘价上加5发出限价单
+            # 为了保证成交，在K线收盘价上加10发出限价单
             price = bar.close_price + 10
             vt_order_ids = self.buy(price, change_volumn)
             self.output("buy vt_order_ids : %s" % str(vt_order_ids))
-            # 当前持有空头仓位，则先平空，再开多
+        # 当前持有空头仓位，则先平空
         elif self.pos < 0 and change_volumn > 0:
             price = bar.close_price + 10
             vt_order_ids = self.cover(price, change_volumn)
             self.output("cover: %s" % str(vt_order_ids))
+        # 多单持仓，仓位减小，平多
         elif self.pos > 0 and change_volumn < 0:
             price = bar.close_price - 10
             self.sell(price, -change_volumn)
+        # 空单持仓，仓位数值减少，即空单增加，开空
         elif self.pos <= 0 and change_volumn < 0:
             price = bar.close_price - 10
             self.short(price, -change_volumn)
 
+        # vnpy内置方法，用于触发客户端UI组件更新，可写可不写
         self.put_event()
 
